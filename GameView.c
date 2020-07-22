bde75303b0656c3ab8f129c6d7c78dadda5f7e0a
AustinHWalsh <austinhwalsh@gmail.com>
2020-07-23 01:44:55 +1000

    start of new gameview in gameview.c

--- GameView.c
+++ GameView.c
@@ -13,29 +13,31 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "Game.h"
 #include "GameView.h"
 #include "Map.h"
 #include "Places.h"
 // add your own #includes here
-
+#define PLACE_POS 6
+#define PLAYER_POS 7
+#define ROUND_DIFF 8
 // TODO: ADD YOUR OWN STRUCTS HERE
 typedef struct playerInfo {
-	Player name;			// player's name
-	int health;				// health of the player
-	int score;				// player's individual score
-	Place currLocation;	// the player's location
+	Player name;				 // player's name
+	int health;					 // health of the player
+	Place currLocation;			 // the player's location
+	Place prevMoves[TRAIL_SIZE]; // the player's previous trail
 } PlayerInfo;
 
 struct gameView {
-	// TODO: ADD FIELDS HERE
-	Round roundNum;
-	int gameScore;
-	Place prevMoves[TRAIL_SIZE];
-	Player currPlayer;
-	Map m;
-	PlayerInfo allPlayers[NUM_PLAYERS];
+	Round roundNum;							// number of rounds that have passed
+	int gameScore;							// current game score
+	char *pastPlays; 						// string of past plays !!!THIS NEEDS TO BE TALKED ABOUT!!!
+	Player currPlayer;						// the "name" of the current player
+	Map m;									// graph of the locations
+	PlayerInfo allPlayers[NUM_PLAYERS];		// array of player informations
 };
 
 ////////////////////////////////////////////////////////////////////////
@@ -43,12 +45,43 @@ struct gameView {
 
 GameView GvNew(char *pastPlays, Message messages[])
 {
-	// TODO: REPLACE THIS WITH YOUR OWN IMPLEMENTATION
-	GameView new = malloc(sizeof(*new));
+	// create new game view
+	GameView new = malloc(sizeof(struct gameView));
 	if (new == NULL) {
 		fprintf(stderr, "Couldn't allocate GameView!\n");
 		exit(EXIT_FAILURE);
 	}
+	
+	int playLen = strlen(pastPlays);
+	new->roundNum = playLen / 7;
+	new->gameScore = GAME_START_SCORE;
+
+	///////////////////////////////////////////
+	//				NOT SURE				 //
+	//				ABOUT THIS				 //
+	///////////////////////////////////////////
+	memcpy(new->pastPlays, pastPlays, playLen);
+		
+	new->m = MapNew();
+	
+	// initialise most of the player info in the arrays
+	for (int i = 0; i < NUM_PLAYERS; i++) {
+		new->allPlayers[i].name = PLAYER_LORD_GODALMING + i;
+		if (new->allPlayers[i].name != PLAYER_DRACULA)
+			new->allPlayers[i].health = GAME_START_HUNTER_LIFE_POINTS;
+		else
+			new->allPlayers[i].health = GAME_START_BLOOD_POINTS;
+		new->allPlayers[i].currLocation.id = GvGetPlayerLocation(new, new->allPlayers[i].name);
+		new->allPlayers[i].currLocation.name = placeIdToName(new->allPlayers[i].currLocation.id);
+		new->allPlayers[i].currLocation.abbrev = placeIdToAbbrev(new->allPlayers[i].currLocation.id);
+		new->allPlayers[i].currLocation.type = placeIdToType(new->allPlayers[i].currLocation.id);
+	}
+	
+	///////////////////////////////////////////
+	//				NEED TO ADD				 //
+	//				TRAILS NEXT				 //
+	///////////////////////////////////////////
+	completePastPlays(new, pastPlays);
 
 	return new;
 }
@@ -197,3 +230,84 @@ PlaceId *GvGetReachableByType(GameView gv, Player player, Round round,
 // Your own interface functions
 
 // TODO
+
+// clean up and complete the remaining required gv elements,
+// such as current score and player health
+// dependencies on the pastPlays string
+void completePastPlays(GameView gv, char *pastPlays) {
+
+	// do actions from each round
+	for (int i = 0; i < gv->roundNum; i++) {
+		// array position of the start of the round (players name)
+		int startOfRound = pastPlays[i * ROUND_DIFF];
+
+		// get current round's player
+		Player roundPlayer;
+		switch(startOfRound) {
+			case 'G':
+				roundPlayer = PLAYER_LORD_GODALMING;
+				break;
+			case 'S':
+				roundPlayer = PLAYER_DR_SEWARD;
+				break;
+			case 'H':
+				roundPlayer = PLAYER_VAN_HELSING;
+				break;
+			case 'M':
+				roundPlayer = PLAYER_MINA_HARKER;
+				break;
+			case 'D':
+				roundPlayer = PLAYER_DRACULA;
+				// dracula's turn reduces point by 1
+				gv->gameScore -= SCORE_LOSS_DRACULA_TURN;
+				break;
+		}
+		gv->currPlayer = roundPlayer;
+
+		// not dracula
+		if (roundPlayer != PLAYER_DRACULA) {
+			// check each action in the round
+			for (int i = 0; pastPlays[startOfRound+i] != '.'; i++) {
+				switch(startOfRound+i) {
+					case 'T':
+						gv->allPlayers[roundPlayer].health -= LIFE_LOSS_TRAP_ENCOUNTER;
+						break;
+					case 'D':
+						gv->allPlayers[roundPlayer].health -= LIFE_LOSS_DRACULA_ENCOUNTER;
+						gv->allPlayers[PLAYER_DRACULA].health -= LIFE_LOSS_HUNTER_ENCOUNTER;
+						break;
+				}
+			}
+
+			if (gv->allPlayers[roundPlayer].health <= 0) {
+				/* move player to the hospital */
+				// TODO
+				gv->allPlayers[roundPlayer].health = 0;
+				gv->gameScore -= SCORE_LOSS_HUNTER_HOSPITAL;
+			}
+
+			// when the player didnt move and didnt just get sent to hospital
+			if (gv->allPlayers[roundPlayer].prevMoves[0].name == gv->allPlayers[roundPlayer].prevMoves[1].name
+				&& gv->allPlayers[roundPlayer].health > 0)
+				gv->allPlayers[roundPlayer].health += LIFE_GAIN_REST;
+
+			if (gv->allPlayers[roundPlayer].health > GAME_START_HUNTER_LIFE_POINTS)
+				gv->allPlayers[roundPlayer].health = GAME_START_HUNTER_LIFE_POINTS;
+
+		} else { // player is dracula
+			// check each action in the round
+			for (int i = 0; i < 4; i++) {
+				if (pastPlays[startOfRound+i] == 'V')
+						gv->gameScore -= SCORE_LOSS_VAMPIRE_MATURES;
+			}
+
+			// if drac just moved to the sea
+			/* i think this is enough? */
+			if (gv->allPlayers[roundPlayer].currLocation.id == SEA_UNKNOWN)
+				gv->allPlayers[roundPlayer].health -= LIFE_LOSS_SEA;
+
+			else if (gv->allPlayers[roundPlayer].currLocation.id == CASTLE_DRACULA)
+				gv->allPlayers[roundPlayer].health += LIFE_GAIN_CASTLE_DRACULA;
+		}
+	}
+}
