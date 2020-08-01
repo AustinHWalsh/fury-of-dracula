65f44c1b7ce2b6dd26030654fccf34f4f4e0fb28
z5310223 <z5310223@ad.unsw.edu.au>
2020-08-01 22:15:39 +1000

    added range function.

--- Part2-AIs/aiUtils.c
+++ Part2-AIs/aiUtils.c
@@ -7,8 +7,27 @@
 #include "aiUtils.h"
 
 //add defines
+#define a 16807
+#define c 0
+#define m (2^31 - 1)
+
+
+//declare local functions
+
+unsigned int random();
+
 
 //initialise functions 
+
+//return a random number
+unsigned int random() {
+    //set X as a random number
+    static unsigned int X;
+    srand(X)
+    X = 0 ? srand(X) : X;
+    X = (a * X + c) % m;
+    return X;
+}
 
 int randomRange(int arrayLen) {
     // call random and get range
