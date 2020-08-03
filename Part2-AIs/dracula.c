7e85ccbc4f25f6c6cc1a26eccdc8bf78de853676
AustinHWalsh <austinhwalsh@gmail.com>
2020-08-03 18:09:06 +1000

    style changes and start of move drac away from sea

--- Part2-AIs/dracula.c
+++ Part2-AIs/dracula.c
@@ -15,6 +15,7 @@
 #include "DraculaView.h"
 #include "Game.h"
 #include "aiUtils.h"
+#define DRAC_NEXT_MOVE reachable[moveNum] 
 
 void decideDraculaMove(DraculaView dv)
 {
@@ -25,32 +26,34 @@ void decideDraculaMove(DraculaView dv)
 		return;
 	}
 	
-	int num = 0;
-	PlaceId *validMoves = DvGetValidMoves(dv, &num);
+	int numOfReach = 0, count;
+	PlaceId *validMoves = DvGetValidMoves(dv, &numOfReach);
 
 	// no valid moves must teleport
-	if (num == 0) 
+	if (numOfReach == 0) 
 		registerBestPlay("TP", "haha!"); 
 	
-	num = 0;
-	PlaceId *reachable = DvWhereCanIGo(dv, &num);
+	numOfReach = 0;
+	PlaceId *reachable = DvWhereCanIGo(dv, &numOfReach);
 
-	int moveNum = randomRange(num);
-	char *loc = PLACES[reachable[moveNum]].abbrev;
+	int moveNum = randomRange(numOfReach);
 	// no places to go, just double back/hide
-	if (num == 0) {
-		registerBestPlay((char *)placeIdToAbbrev(validMoves[num]), "haha!"); 
+	if (numOfReach == 0) {
+		registerBestPlay(placeIdToAbbrev(validMoves[numOfReach]), "haha!"); 
 		return;
 	}
-		
-	//if random location is already occupied by hunter, randomise again
-	for (int i = PLAYER_LORD_GODALMING; i < PLAYER_DRACULA; i++) {
-		if (DvGetPlayerLocation(dv, i) == reachable[moveNum]) {
-			moveNum = randomRange(num);
-			loc = PLACES[reachable[moveNum]].abbrev;
-			break;
-		} 
+
+	// if dracula has less than 10 health, try to get him to move
+	// away from the sea
+	if (DvGetHealth(dv, PLAYER_DRACULA) <= 10) {
+		// find a move in his reachable that isnt the sea
+		while (placeIdToType(DRAC_NEXT_MOVE) == SEA) {
+			if (moveNum+count > numOfReach) {
+					
+			}
+		}
 	}
+	
 
 	// if the location is in the hunter's reachable, randomise again
 	for (int i = 0; i < NUM_PLAYERS-1; i++) {
@@ -59,14 +62,21 @@ void decideDraculaMove(DraculaView dv)
 		PlaceId *hunterMoves = DvWhereCanTheyGo(dv, player, &locs);
 		// test each reachable 
 		for (int j = 0; j < locs; j++) {
-			if (reachable[moveNum] == hunterMoves[j]) {
-				moveNum = randomRange(num);
-				loc = PLACES[reachable[moveNum]].abbrev;
+			if (DRAC_NEXT_MOVE == hunterMoves[j]) {
+				moveNum = randomRange(numOfReach);
 				break;
 			}
 		}
 	}
 
+	//if random location is already occupied by hunter, randomise again
+	for (int i = PLAYER_LORD_GODALMING; i < PLAYER_DRACULA; i++) {
+		if (DvGetPlayerLocation(dv, i) == DRAC_NEXT_MOVE) {
+			moveNum = randomRange(numOfReach);
+			break;
+		} 
+	}
+
 	// move to random location
-	registerBestPlay(loc, "ha!"); 
+	registerBestPlay(placeIdToAbbrev(DRAC_NEXT_MOVE), "ha!"); 
 }
